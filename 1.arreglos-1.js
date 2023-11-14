bb0af3e36229a57efa96d346b69e1c69a437029f
Cristian Garzon <cristian.garzon@masiv.com>
2023-11-14 15:26:03 -0500

    Agregar ejercicios de arreglos

---
+++ 1.arreglos-1.js
@@ -0,0 +1,142 @@
+/*
+  1. Teniendo un arreglo de "cosas", filtrar solo los elementos que sean cadenas de texto 
+  que tengan al menos una letra SIN USAR funciones de los arreglos, en otras palabras,
+  usando un ciclo for, while u otro.
+*/
+
+const cosas1 = ["manzana", true, "", "pera", 123, {}, [], "hola", ""];
+
+const filtrarArreglo1 = (listadoDeCosas) => {
+  let listadoFiltrado = [];
+  // escribir el codigo aquí...
+  return listadoFiltrado;
+};
+
+console.log("Resultado ejercicio 1.", filtrarArreglo1(cosas1));
+
+/* 
+  2. Teniendo un arreglo de "cosas", filtrar solo los elementos que sean cadenas de texto 
+  que tengan al menos una letra USANDO funciones de los arreglos.
+*/
+
+const cosas2 = ["manzana", true, "", "pera", 123, {}, [], "hola", ""];
+
+const filtrarArreglo2 = (listadoDeCosas) => {
+  let listadoFiltrado = [];
+  // escribir el codigo aquí...
+  return listadoFiltrado;
+};
+
+console.log("Resultado ejercicio 2.", filtrarArreglo2(cosas2));
+
+/* 
+  NOTA:
+  De ahora en adelante, A MENOS QUE SE ESPECIFIQUE, no es necesario que de manera estricta 
+  se usen ciclos for, while, etc o funciones de arreglos, queda a preferencia.
+*/
+
+/* 
+  3. Teniendo un arreglo de "cosas", filtrar solo los elementos que sean un booleano false
+*/
+
+const cosas3 = [
+  "manzana",
+  true,
+  null,
+  undefined,
+  false,
+  123,
+  0,
+  [],
+  false,
+  false,
+];
+
+const filtrarArreglo3 = (listadoDeCosas) => {
+  let listadoFiltrado = [];
+  // escribir el codigo aquí...
+  return listadoFiltrado;
+};
+
+console.log("Resultado ejercicio 3.", filtrarArreglo3(cosas3));
+
+/* 
+  4. Teniendo un arreglo de numeros, ordenar de mayor a menor.
+*/
+
+const numeros4 = [2, 4, 6, 1, 0, 12, 30, 30];
+
+const ordenarArreglo4 = (listadoDeNumeros) => {
+  let listadoOrdenado = [];
+  // escribir el codigo aquí...
+  return listadoOrdenado;
+};
+
+console.log("Resultado ejercicio 4.", ordenarArreglo4(numeros4));
+
+/* 
+  5. Teniendo un arreglo de numeros, sumar todos los valores.
+*/
+
+const numeros5 = [2, 4, 6, 1, 0, 12, 30, 30];
+
+const acumularArreglo = (listadoDeNumeros) => {
+  let acumulado = 0;
+  // escribir el codigo aquí...
+  return acumulado;
+};
+
+console.log("Resultado ejercicio 5.", acumularArreglo(numeros5));
+
+/* 
+  6. Teniendo una palabra, convertirla a arreglo, filtrar solo las vocales, 
+  después convertir de arreglo a cadena de texto de nuevo.
+*/
+
+const palabra6 = "pruebapalabraconsonantes";
+
+const convertirPalabra = (palabra) => {
+  // escribir codigo aquí...
+};
+
+console.log("Resultado ejercicio 6.", convertirPalabra(palabra6));
+
+/* 
+  7. Teniendo un arreglo de numeros, eliminar los duplicados.
+*/
+
+const numeros7 = [0, 1, 5, 8, 2, 3, 6, 9, 3, 2, 2, 1, 10, 8];
+
+const eliminarDuplicados7 = () => {
+  // escribir codigo aquí...
+};
+
+console.log("Resultado ejercicio 7.", eliminarDuplicados7(numeros7));
+
+/* 
+  8. Teniendo un arreglo de numeros, retornar un arreglo de cada numero multiplicado por 2.
+*/
+
+const numeros8 = [9, 23, 2, 43, 59, 12];
+
+const multiplicarNumeros8 = () => {
+  // escribir codigo aquí...
+};
+
+console.log("Resultado ejercicio 8.", multiplicarNumeros8(numeros8));
+
+/* 
+  9. Teniendo un arreglo de arreglos, ordenar de menor a mayor los elementos de cada arreglo.
+*/
+
+const arreglos9 = [
+  [1, 45, 27, 98, 24],
+  [8, 23, 65, 123, 6],
+  [72, 12, 53, 73, 2],
+];
+
+const ordenarArreglos9 = (listadoArreglos) => {
+  // escribir codigo aquí...
+};
+
+console.log("Resultado ejercicio 9.", ordenarArreglos9(arreglos9));
