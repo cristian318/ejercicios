14489fadb543394b2b1b873b0a769a42f37b075e
Cristian Garzon <cristian.garzon@masiv.com>
2023-11-21 12:36:14 -0500

    Agregar ejercicios de objetos

---
+++ 2.objetos-1.js
@@ -0,0 +1,142 @@
+/*
+  1. Teniendo un objeto con propiedades de persona, imprimir solo el nombre.
+*/
+
+const persona1 = {
+  nombre: "Juan",
+  apellido: "Perez",
+  edad: 20,
+  peso: 70,
+  altura: 1.8,
+};
+
+/* 
+  2. Teniendo un objeto con propiedades de persona, imprimir solo el nombre y apellido.
+*/
+
+const persona2 = {
+  nombre: "Juan",
+  apellido: "Perez",
+  edad: 20,
+  peso: 70,
+  altura: 1.8,
+};
+
+/* 
+  3. Teniendo un arreglo de objetos con propiedades de persona, retornar un arreglo de solo nombres.
+*/
+
+const personas3 = [
+  {
+    nombre: "Juan",
+    apellido: "Perez",
+    edad: 20,
+    peso: 70,
+    altura: 1.8,
+  },
+  {
+    nombre: "Maria",
+    apellido: "Perez",
+    edad: 20,
+    peso: 70,
+    altura: 1.8,
+  },
+  {
+    nombre: "Pedro",
+    apellido: "Perez",
+    edad: 20,
+    peso: 70,
+    altura: 1.8,
+  },
+];
+
+/* 
+  4. Teniendo un arreglo de objetos con propiedades de persona, retornar un arreglo de solo nombres y 
+  apellidos.
+*/
+
+const personas4 = [
+  {
+    nombre: "Juan",
+    apellido: "Perez",
+    edad: 20,
+    peso: 70,
+    altura: 1.8,
+  },
+  {
+    nombre: "Maria",
+    apellido: "Perez",
+    edad: 20,
+    peso: 70,
+    altura: 1.8,
+  },
+  {
+    nombre: "Pedro",
+    apellido: "Perez",
+    edad: 20,
+    peso: 70,
+    altura: 1.8,
+  },
+];
+
+/* 
+  5. Crea un objeto llamado persona con propiedades como nombre, edad y profesion. Asigna valores y 
+  muestra la información en la consola.
+*/
+
+/* 
+  6. Modifica la edad de la persona creada en el ejercicio anterior. Muestra la información actualizada 
+  en la consola.
+*/
+
+/*
+  7. Agrega un método llamado saludar al objeto persona que imprima en la consola un saludo personalizado 
+  utilizando sus propiedades.
+*/
+
+/* 
+  8. Crea una función que recorra todas las propiedades del objeto persona y las muestre en la consola.
+*/
+
+/* 
+  9. Crea un nuevo objeto llamado direccion con propiedades como calle y ciudad. Agrega este objeto 
+  como una propiedad al objeto persona y muestra la información completa.
+*/
+
+/*
+  10. Modifica el objeto persona del ejercicio básico para incluir una propiedad email. Crea una función 
+  que valide si el formato del email es correcto.
+*/
+
+/*
+  11. Crea una función constructora que permita crear objetos producto con propiedades como nombre, 
+  precio y stock. Luego, crea varios productos dinámicamente y muestra la información en la consola.
+*/
+
+/* 
+  12. Teniendo un arreglo de objetos producto, crea una función que permita buscar un producto por su
+  nombre. La función debe retornar el producto encontrado.
+*/
+
+/* 
+  13. Teniendo un arreglo de objetos tienda con propiedades como nombre y productos, crea una función
+  que permita retornar todos los productos de todas las tiendas, agregando el nombre de la tienda a cada
+  producto.
+
+*/
+
+/* 
+  14. Teniendo un arreglo de objetos tienda con propiedades como nombre y productos, crea una función
+  que permita retornar un objeto con los nombres de las tiendas como propiedades y que cada propiedad
+  contenga el numero de productos de la tienda.
+  {
+    tienda1: 10,
+    tienda2: 20,
+    tienda3: 30,
+  }
+*/
+
+/* 
+  15. Teniendo el objeto del ejercicio anterior, crea una función que permita retornar un arreglo con
+  todas las tiendas ordenadas por la cantidad de productos que tienen.
+*/
